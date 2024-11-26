0f0764eccbfbd5b4731eb7a9225866bfb8bfa2be
unagumoro <tomaz.schorsch@gmail.com>
2024-11-26 20:32:29 -0300

    3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.room:room-common:2.6.1")
 
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
--- app/src/main/java/br/edu/up/rgm34305491/data/Item.kt
+++ app/src/main/java/br/edu/up/rgm34305491/data/Item.kt
@@ -16,11 +16,16 @@
 
 package br.edu.up.rgm34305491.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
+@Entity(tableName = "items")
+
+data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
